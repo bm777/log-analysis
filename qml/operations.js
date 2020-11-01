0496f746f0d686a7e8722afe885119cbe2d20f07
Bayangmbe Mounmo <bayangp0@gmail.com>
2020-11-01 16:19:24 +0100

    Add load URL function

--- qml/operations.js
+++ qml/operations.js
@@ -3,7 +3,8 @@
 
 function check(filename) {
     var str = filename.toString();
-        return str.endsWith(".jpg") ? true : false;
+        return str.endsWith(".xlsx") ? true : false;
+    console.log(".xlsx");
 }
 
 
